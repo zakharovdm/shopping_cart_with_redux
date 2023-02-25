195850efbeffd708696286bee93ca1a65372d1c0
Dmitriy Zakharov <dzaharov794@gmail.com>
2023-02-26 01:05:34 +0300

    add cart slice to components

--- src/components/Cart/CartButton.js
+++ src/components/Cart/CartButton.js
@@ -1,19 +1,19 @@
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { uiActions } from '../../store/ui-slice';
 import classes from './CartButton.module.css';
 
 const CartButton = (props) => {
   const dispatch = useDispatch();
+  const cartQuantity = useSelector((state) => state.cart.totalQuantity)
 
   const toggleCartHandler = () => {
     dispatch(uiActions.toogleCart());
   }
 
-
   return (
     <button className={classes.button} onClick={toggleCartHandler}>
       <span>My Cart</span>
-      <span className={classes.badge}>1</span>
+      <span className={classes.badge}>{cartQuantity}</span>
     </button>
   );
 };
